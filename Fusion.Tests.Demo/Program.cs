d80c02703a82b3e896dfbd099254322a3d624a3d
Lamparter <notlamparter@outlook.com>
2024-09-17 18:13:13 +0100

    🎉 Add demo project

---
+++ Fusion.Tests.Demo/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var processor = new MarkdownProcessor(MarkdownStandard.CommonMark);
+        var markdown = @"Change this to whatever you want to test on demand!";
+        string html = processor.ConvertMarkdownToHtml(markdown);
+        Console.WriteLine(html);
+    }
+}
